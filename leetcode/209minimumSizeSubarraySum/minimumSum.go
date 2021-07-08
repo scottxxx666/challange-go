d7f41502623baaee002f68d61771a17ff03782b1
Scott Kao <scottxxx666@gmail.com>
2021-07-09 01:42:54 +0800

    [refactor] shorter

--- leetcode/209minimumSizeSubarraySum/minimumSum.go
+++ leetcode/209minimumSizeSubarraySum/minimumSum.go
@@ -3,16 +3,14 @@ package _09minimumSizeSubarraySum
 func minSubArrayLen(target int, nums []int) int {
 	res := len(nums) + 1
 	sum := 0
-	for i, j := 0, 0; i <= j && j < len(nums); {
+	for i, j := 0, 0; i <= j && j < len(nums); j++ {
 		sum += nums[j]
-		if sum >= target {
+		for sum >= target {
 			if j-i+1 < res {
 				res = j - i + 1
 			}
-			sum -= nums[i] + nums[j]
+			sum -= nums[i]
 			i++
-		} else {
-			j++
 		}
 	}
 
