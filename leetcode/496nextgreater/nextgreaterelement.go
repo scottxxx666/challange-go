1f61bba59c5574d4479d218aa80bd3edefa46961
Scott Kao <scottxxx666@gmail.com>
2021-07-10 02:51:06 +0800

    [refactor] less memory

--- leetcode/496nextgreater/nextgreaterelement.go
+++ leetcode/496nextgreater/nextgreaterelement.go
@@ -1,23 +1,26 @@
 package _96nextgreater
 
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
-	m := make(map[int]int)
+	indexes := make(map[int]int)
+	for i := 0; i < len(nums1); i++ {
+		indexes[nums1[i]] = i
+	}
+
+	res := make([]int, len(nums1))
 	stack := make([]int, 0)
 	for i := len(nums2) - 1; i >= 0; i-- {
 		for len(stack) > 0 && nums2[i] > stack[len(stack)-1] {
 			stack = stack[:len(stack)-1]
 		}
-		if len(stack) == 0 {
-			m[nums2[i]] = -1
-		} else {
-			m[nums2[i]] = stack[len(stack)-1]
+		if index, ok := indexes[nums2[i]]; ok {
+			if len(stack) == 0 {
+				res[index] = -1
+			} else {
+				res[index] = stack[len(stack)-1]
+			}
 		}
 		stack = append(stack, nums2[i])
 	}
 
-	res := make([]int, len(nums1))
-	for i := 0; i < len(nums1); i++ {
-		res[i] = m[nums1[i]]
-	}
 	return res
 }
