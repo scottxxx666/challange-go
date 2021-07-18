8b91deda781320024fd8b2f187d11619d02c6cd9
Scott Kao <scottxxx666@gmail.com>
2021-07-19 02:23:48 +0800

    [refactor] DP solution

--- leetcode/279perfectsquare/numsquares.go
+++ leetcode/279perfectsquare/numsquares.go
@@ -5,15 +5,12 @@ import "math"
 func numSquares(n int) int {
 	sums := make([]int, n+1)
 	for i := 1; i <= n; i++ {
-		s := math.Sqrt(float64(i))
-		if s == float64(int(s)) {
-			sums[i] = 1
-			continue
-		}
 		temp := i
-		for j := 1; j <= int(math.Ceil(float64(i/2))); j++ {
-			if sums[j]+sums[i-j] < temp {
-				temp = sums[j] + sums[i-j]
+		for j := 1; j*j <= i; j++ {
+			if i == j*j {
+				temp = 1
+			} else if 1+sums[i-j*j] < temp {
+				temp = 1 + sums[i-j*j]
 			}
 		}
 		sums[i] = temp
